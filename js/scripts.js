4b2357e0656b3bf9625d01726b4c8950e944337a
ramza007 <ramzalvo007@gmail.com>
2018-03-07 10:53:03 +0300

    js changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,10 @@
-var foodieContainer = document.getElementById("food");
-var foodie = document.getElementById("foodie");
+/*
+$.ajax({
+    method: 'GET',
+    url: "https://api.edamam.com/search?q=chicken&app_id=c35f9e5d&app_key=529da2c991301ddb59c8c90d103b8d05",
+    success: function (food) {
+        console.log(food.hits[0].recipe.label).innerHTML;
+    });
+    */
 
-foodie.addEventListener("click", function() {
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://www.edamam.com/c35f9e5d');
-    ourRequest.onload = function () {
-        var ourData = JSON.parse(ourRequest.responseText);
-        renderHTML(ourData);
-    };
-    ourRequest.send();
-});
-function renderHTML(data) {
-    foodieContainer.insertAdjacentHTML('beforeend', 'testing 123');
-}
+window.alert ("contains nuts!")
